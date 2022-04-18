cee7ea00326e2c6b24d01a2cdac298368d90fd41
Chanwoo Lee <leechanwoo25@gmail.com>
2022-04-18 08:02:06 +0000

    Simplify Net's parameters

--- src/GyroNet.py
+++ src/GyroNet.py
@@ -20,51 +20,43 @@ def vis(xs:torch.Tensor):
 
 
 class DGANet(torch.nn.Module):
-    """It's a DNN Model for denoising IMU sensor\'s raw measurements (both gryo & accel)"""
-    def __init__(self, params):
+    def __init__(self, in_dim, out_dim, c0, dropout, ks, ds, momentum, gyro_std, acc_std):
         super().__init__()
-        print('\n# Initilaize DGANet ...')
+        cprint('Initilaize DGANet ...', 'green')
+        print("  It's a DNN Model for denoising IMU sensor\'s raw measurements (both gryo & accel)")
 
-        net_params = params['net']
-
-        self.in_dim  = net_params['in_dim']
-        self.out_dim = net_params['out_dim']
-        self.dropout = net_params['dropout']
-        self.momentum= net_params['momentum']
-        self.gyro_std = net_params['gyro_std']
-        self.acc_std = net_params['acc_std']
-
+        self.in_dim = in_dim
+        self.out_dim = out_dim
         # channel dimension
-        c0 = net_params['c0']
         c1 = 2*c0
         c2 = 2*c1
         c3 = 2*c2
         # kernel dimension (odd number)
-        k0 = net_params['ks'][0]
-        k1 = net_params['ks'][1]
-        k2 = net_params['ks'][2]
-        k3 = net_params['ks'][3]
+        k0 = ks[0]
+        k1 = ks[1]
+        k2 = ks[2]
+        k3 = ks[3]
         # dilation dimension
-        d0 = net_params['ds'][0]
-        d1 = net_params['ds'][1]
-        d2 = net_params['ds'][2]
+        d0 = ds[0]
+        d1 = ds[1]
+        d2 = ds[2]
         # padding
         p0 = (k0-1) + d0*(k1-1) + d0*d1*(k2-1) + d0*d1*d2*(k3-1)
 
         # todo
         # 1. Bias = False Try
-        self.conv1 = torch.nn.Conv1d(self.in_dim, c0, k0, dilation=1)
+        self.conv1 = torch.nn.Conv1d(in_dim, c0, k0, dilation=1)
         self.conv2 = torch.nn.Conv1d(c0, c1, k1, dilation=d0)
         self.conv3 = torch.nn.Conv1d(c1, c2, k2, dilation=d0*d1)
         self.conv4 = torch.nn.Conv1d(c2, c3, k3, dilation=d0*d1*d2)
-        self.conv5 = torch.nn.Conv1d(c3, self.out_dim, 1, dilation=1)
+        self.conv5 = torch.nn.Conv1d(c3, out_dim, 1, dilation=1)
 
         self.gelu = torch.nn.GELU()
-        self.dropout = torch.nn.Dropout(self.dropout)
-        self.bn1 = torch.nn.BatchNorm1d(c0, momentum=self.momentum)
-        self.bn2 = torch.nn.BatchNorm1d(c1, momentum=self.momentum)
-        self.bn3 = torch.nn.BatchNorm1d(c2, momentum=self.momentum)
-        self.bn4 = torch.nn.BatchNorm1d(c3, momentum=self.momentum)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.bn1 = torch.nn.BatchNorm1d(c0, momentum=momentum)
+        self.bn2 = torch.nn.BatchNorm1d(c1, momentum=momentum)
+        self.bn3 = torch.nn.BatchNorm1d(c2, momentum=momentum)
+        self.bn4 = torch.nn.BatchNorm1d(c3, momentum=momentum)
 
         self.replicationPad1dS = torch.nn.ReplicationPad1d((p0, 0))
         self.replicationPad1dE = torch.nn.ReplicationPad1d((0, 0))
@@ -74,10 +66,10 @@ class DGANet(torch.nn.Module):
         self.C_w = torch.nn.Parameter(torch.randn(3, 3)*5e-2)
         self.C_a = torch.nn.Parameter(torch.randn(3, 3)*5e-2)
         # Not trainable
-        self.mean_u     = torch.nn.Parameter(torch.zeros(self.in_dim),      requires_grad=False)
-        self.std_u      = torch.nn.Parameter(torch.ones(self.in_dim),       requires_grad=False)
-        self.gyro_std   = torch.nn.Parameter(torch.Tensor(self.gyro_std),   requires_grad=False)
-        self.acc_std    = torch.nn.Parameter(torch.Tensor(self.acc_std),    requires_grad=False)
+        self.mean_u     = torch.nn.Parameter(torch.zeros(in_dim),      requires_grad=False)
+        self.std_u      = torch.nn.Parameter(torch.ones(in_dim),       requires_grad=False)
+        self.gyro_std   = torch.nn.Parameter(torch.Tensor(gyro_std),   requires_grad=False)
+        self.acc_std    = torch.nn.Parameter(torch.Tensor(acc_std),    requires_grad=False)
         self.I3          = torch.nn.Parameter(torch.eye(3),            requires_grad=False)
         self.g          = torch.nn.Parameter(torch.Tensor([0,0,9.81]), requires_grad=False) # Alse can be 9.81 | Is there any difference on results?
         ###
@@ -86,7 +78,7 @@ class DGANet(torch.nn.Module):
         self.gt_rots = None
         ###
 
-        print("--- success ---\n")
+        print("  -- success --\n")
 
     def set_normalized_factors(self, mean_u, std_u):
         self.mean_u = torch.nn.Parameter(mean_u.cuda(), requires_grad=False)
@@ -157,33 +149,33 @@ class DGANet(torch.nn.Module):
             # a_hat: torch.Size([6, 16000, 3]) torch.float32
         return w_hat, a_hat
 
-params = {
-    'net': {
-        'in_dim': 6,
-        'out_dim': 6,
-        'c0': 16,
-        'dropout': 0.1,
-        'ks': [7, 7, 7, 7],
-        'ds': [4, 4, 4],
-        'momentum': 0.1,
-        'gyro_std': [1*np.pi/180, 2*np.pi/180, 5*np.pi/180],
-        'acc_std': [2.0e-3, 2.0e-3, 2.0e-3],
-    }
+net_params = {
+    'in_dim': 6,
+    'out_dim': 6,
+    'c0': 16,
+    'dropout': 0.1,
+    'ks': [7, 7, 7, 7],
+    'ds': [4, 4, 4],
+    'momentum': 0.1,
+    'gyro_std': [1*np.pi/180, 2*np.pi/180, 5*np.pi/180],
+    'acc_std': [2.0e-3, 2.0e-3, 2.0e-3],
 }
 
 if __name__ == "__main__":
 
-    net = DGANet(params).cuda()
+    net = DGANet(**net_params).cuda()
     print(net)
+    # vis([net.mean_u, net.std_u, net.gyro_std, net.Id3])
 
-    input = torch.randn((1, 16000, 6)).cuda()
-    rot   = torch.randn((1, 16000, 3, 3)).cuda()
+    input = torch.randn((6, 16000, 6)).cuda()
     print(input.shape)
 
     net.eval()
-    w_hat, a_hat = net(input, rot)
+    w_hat, a_hat = net(input)
 
     w_hat = w_hat.cpu().detach()
     a_hat = a_hat.cpu().detach()
     print("w_hat:", w_hat.shape, w_hat.dtype, w_hat.device, w_hat.requires_grad)
     print("a_hat:", a_hat.shape, a_hat.dtype, a_hat.device, a_hat.requires_grad)
+    # w_hat = w_hat.detach().numpy()
+    # a_hat = a_hat.detach().numpy()
